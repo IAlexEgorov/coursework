2c9e139ae7e0cf7136a9b2722a61300f970b6ca6
Alex Egorov <sasha-egorov-01@list.ru>
2022-03-13 14:02:41 +0300

    Add ldap-check

---
+++ README.md
@@ -0,0 +1,6 @@
+# Авторизация 
+Взято с сайта: https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login-ru
+
+Ютуб: https://www.youtube.com/watch?v=24vVFtwuBWs
+
+Еще: https://learntutorials.net/ru/flask/topic/9053/%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F-%D0%B8-%D0%B0%D1%83%D1%82%D0%B5%D0%BD%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%86%D0%B8%D1%8F
--- app.py
+++ app.py
@@ -1,14 +1,68 @@
-from flask import Flask
-from flask import render_template
-import os 
+from traceback import print_tb
+from flask import Flask, render_template, request
+import requests 
+import sqlite3
+import subprocess
+
+#class UserLogin:
+#    def fromDB(self, user_id, db):
+#        self.__user = db.getUser(user_id)
+#        return self
+#
+#    def is_authenticated():
+#        return True 
+#    
+#    def is_activ():
+#        return True
+#
+#    def is_anonymous(self):
+#        return False
+#
+#    def get_id(self):
+#        return str(self.__user['id'])
 
 app = Flask(__name__)
 
+#def connect_db():
+#    sqlite_connection = sqlite3.connect(app.config['DATABASE'])
+#    sqlite_connection.row_factory = sqlite3.Row
+#    return sqlite_connection
+
+#def create_db():
+
+
+#login_manager = LoginManager(app)
+
+def checkUser(login, password):
+    req='''ldapsearch 
+    -h ipa.web-bee.loc -p 389 
+    -b uid='''+login+''',cn=users,cn=accounts,dc=web-bee,dc=loc 
+    -D uid='''+login+''',cn=users,cn=accounts,dc=web-bee,dc=loc -w '''+password
+    sub=subprocess.call(req.split())
+    if (sub==0):
+        return True
+    else:
+        return False
+
+def makeStand():
+    req=''''''
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', desc="Создание автостендов для разработчиков и тестеровщиков")
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/', methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        print(request.form)
+        login = request.form["login"]
+        password = request.form["pass"]
+        if checkUser(login, password):
+
+            return render_template('index.html', desc="Успешно")
+        else:
+            return render_template('index.html', desc="Ошибка доступа")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
