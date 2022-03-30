32efe501f9e6757ef225e9e2eda2a7d2d0edc9ca
Alex Egorov <sasha-egorov-01@list.ru>
2022-03-30 12:11:31 +0300

    add test.py in gitignore

--- ldap_test.py
+++
@@ -1,8 +0,0 @@
-from ldap3 import Connection, Server
-
-user = "bot.admin"
-password = "DEVpassword"
-server = Server('ipa.web-bee.loc', use_ssl=True)
-
-conn = Connection(server, user="uid=bot.admin,cn=users,cn=accounts,dc=web-bee,dc=loc", password=password)
-print(conn.entries[0])
